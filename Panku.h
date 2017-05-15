5742723da2c3b3ee2349c683eba860e1fcd99cb3
PabloMansanet <pablomansanet@gmail.com>
2017-05-15 18:27:24 +0200

    Empty branch

--- Panku.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "private/Metaprogram.h"
-
-// Include this file and use the following macros:
-//
-// PANKU_LIST;
-// PANKU_INSTANCE;
-//
-// Check out README.md for instructions.
--- example/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "Devices.h"
-
-int main(void) {
-   panku.Get<Alpha>().Talk();
-   panku.Get<Beta>().Talk();
-   panku.Get<Gamma>().Talk();
-}
